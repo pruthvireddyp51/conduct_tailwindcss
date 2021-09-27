40d7a54ae78c953b89128a4fec8595d3786fdbd1
pruthvi <pruthvireddyp51@gmail.com>
2021-09-27 21:19:10 +0530

    alignment fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,14 @@ module.exports = {
         brand: '#DB3700',
         darkBlue: '#1A0D3F',
         titleBrown: '#681E05',
-        titleBrownLite: '#A37D61'
+        titleBrownLite: '#A37D61',
+        titleBrownMedium: '#DBA15F',
+        tBlue:'rgba(96, 114, 226, 0.128196)',
+        tPink:'rgba(152, 43, 190, 0.128196)',
+        tOrange:'rgba(226, 98, 98, 0.128196)',
+        tGreen:'rgba(70, 194, 125, 0.128196)',
+        tGrey:'rgba(67, 77, 144, 0.128196)',
+        tYellow:'rgba(231, 153, 65, 0.128196)',
       }
     },
   },
